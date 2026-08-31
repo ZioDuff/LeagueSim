29a189745e5d29338e0101bfc8e1c0583bb7a806
Jacopo De maio <jajodemaio@gmail.com>
2026-08-31 16:30:48 +0200

    Initial commit

---
+++ src/main/java/org/generation/italy/LeagueSim/LeagueSimApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.LeagueSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/italy/LeagueSim/LeagueSimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.LeagueSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
